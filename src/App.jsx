3a9cef524d89fa2ee82c9a6a2ab97c007a9fba5a
nathanieladiah <nathanieladiah@gmail.com>
2024-11-22 06:33:10 -0400

    Add setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import PrivateRoute from "./components/PrivateRoute";
+import SignIn from "./pages/SignIn";
 const App = () => {
   return (
     <>
       <Router>
         <Routes>
           <Route path="/" element={<h1>Home</h1>} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route element={<PrivateRoute />}></Route>
         </Routes>
       </Router>
     </>
---
+++ src/pages/SignIn.jsx
@@ -0,0 +1,97 @@
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import ArrowRightIcon from "../assets/svg/keyboardArrowRightIcon.svg?react";
+import VisibilityIcon from "../assets/svg/visibilityIcon.svg";
+import { auth } from "../firebase.config";
+
+const SignIn = () => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const { email, password } = formData;
+
+  const navigate = useNavigate();
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+
+      if (userCredential.user) {
+        navigate("/");
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Invalid User Credentials");
+    }
+  };
+
+  return (
+    <>
+      <div className="pageContainer">
+        <header>
+          <p className="pageHeader">Welcome Back</p>
+        </header>
+
+        <form onSubmit={onSubmit}>
+          <input
+            type="email"
+            className="emailInput"
+            placeholder="Email"
+            id="email"
+            value={email}
+            onChange={onChange}
+          />
+
+          <div className="passwordInputDiv">
+            <input
+              type={showPassword ? "text" : "password"}
+              className="passwordInput"
+              placeholder="Password"
+              id="password"
+              value={password}
+              onChange={onChange}
+            />
+
+            <img
+              src={VisibilityIcon}
+              alt="show password"
+              className="showPassword"
+              onClick={() => setShowPassword((prevState) => !prevState)}
+            />
+          </div>
+
+          <Link to="/" className="forgotPasswordLink">
+            Forgot Password
+          </Link>
+
+          <div className="signInBar">
+            <p className="signInText">Sign In</p>
+            <button className="signInButton">
+              <ArrowRightIcon fill="#ffffff" width="34px" height="34px" />
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+};
+export default SignIn;
